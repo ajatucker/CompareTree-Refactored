bf864092d06a9915740a96ec7ca9c213098fe6e8
Harold Tudtud <haroldns@umich.edu>
2022-04-19 22:58:51 -0400

    Beginning prototype for form

--- CIS376-CompareTrees-TermProject/MyForm.h
+++ CIS376-CompareTrees-TermProject/MyForm.h
@@ -34,6 +34,16 @@ namespace CIS376CompareTreesTermProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ delete_btn;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::Label^ label1;
+
+	protected:
 
 	protected:
 
@@ -50,18 +60,118 @@ namespace CIS376CompareTreesTermProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->delete_btn = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->groupBox1->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(128, 729);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(154, 60);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Insert";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(288, 729);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(154, 60);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Search";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// delete_btn
+			// 
+			this->delete_btn->Location = System::Drawing::Point(448, 728);
+			this->delete_btn->Name = L"delete_btn";
+			this->delete_btn->Size = System::Drawing::Size(154, 60);
+			this->delete_btn->TabIndex = 2;
+			this->delete_btn->Text = L"Delete";
+			this->delete_btn->UseVisualStyleBackColor = true;
+			this->delete_btn->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(128, 697);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(154, 26);
+			this->textBox1->TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(288, 697);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(154, 26);
+			this->textBox2->TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(448, 696);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(154, 26);
+			this->textBox3->TabIndex = 5;
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->label1);
+			this->groupBox1->Location = System::Drawing::Point(128, 231);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(154, 226);
+			this->groupBox1->TabIndex = 6;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"AVL Tree";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(6, 43);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(54, 20);
+			this->label1->TabIndex = 7;
+			this->label1->Text = L"Insert:";
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(488, 399);
+			this->ClientSize = System::Drawing::Size(1259, 1055);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->delete_btn);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		int test = 3;
+
+		label1->Text = "Insert: " + System::Convert::ToString(test);
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+
+};
 }
