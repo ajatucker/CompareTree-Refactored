3e31f38e9df4b7029d3641c666da15ae656607b7
Harold Tudtud <haroldns@umich.edu>
2022-04-19 23:20:13 -0400

    Finished template for form

--- CIS376-CompareTrees-TermProject/MyForm.h
+++ CIS376-CompareTrees-TermProject/MyForm.h
@@ -42,6 +42,16 @@ namespace CIS376CompareTreesTermProject {
 	private: System::Windows::Forms::TextBox^ textBox3;
 	private: System::Windows::Forms::GroupBox^ groupBox1;
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::GroupBox^ groupBox2;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::GroupBox^ groupBox3;
+	private: System::Windows::Forms::Label^ label7;
+	private: System::Windows::Forms::Label^ label8;
+	private: System::Windows::Forms::Label^ label9;
 
 	protected:
 
@@ -67,13 +77,25 @@ namespace CIS376CompareTreesTermProject {
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->groupBox3 = (gcnew System::Windows::Forms::GroupBox());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
 			this->groupBox1->SuspendLayout();
+			this->groupBox2->SuspendLayout();
+			this->groupBox3->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(128, 729);
+			this->button1->Location = System::Drawing::Point(12, 342);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(154, 60);
 			this->button1->TabIndex = 0;
@@ -83,7 +105,7 @@ namespace CIS376CompareTreesTermProject {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(288, 729);
+			this->button2->Location = System::Drawing::Point(172, 342);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(154, 60);
 			this->button2->TabIndex = 1;
@@ -93,7 +115,7 @@ namespace CIS376CompareTreesTermProject {
 			// 
 			// delete_btn
 			// 
-			this->delete_btn->Location = System::Drawing::Point(448, 728);
+			this->delete_btn->Location = System::Drawing::Point(332, 341);
 			this->delete_btn->Name = L"delete_btn";
 			this->delete_btn->Size = System::Drawing::Size(154, 60);
 			this->delete_btn->TabIndex = 2;
@@ -103,34 +125,54 @@ namespace CIS376CompareTreesTermProject {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(128, 697);
+			this->textBox1->Location = System::Drawing::Point(12, 310);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(154, 26);
 			this->textBox1->TabIndex = 3;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(288, 697);
+			this->textBox2->Location = System::Drawing::Point(172, 310);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(154, 26);
 			this->textBox2->TabIndex = 4;
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(448, 696);
+			this->textBox3->Location = System::Drawing::Point(332, 309);
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size(154, 26);
 			this->textBox3->TabIndex = 5;
 			// 
 			// groupBox1
 			// 
+			this->groupBox1->Controls->Add(this->label3);
+			this->groupBox1->Controls->Add(this->label2);
 			this->groupBox1->Controls->Add(this->label1);
-			this->groupBox1->Location = System::Drawing::Point(128, 231);
+			this->groupBox1->Location = System::Drawing::Point(12, 56);
 			this->groupBox1->Name = L"groupBox1";
-			this->groupBox1->Size = System::Drawing::Size(154, 226);
+			this->groupBox1->Size = System::Drawing::Size(154, 148);
 			this->groupBox1->TabIndex = 6;
 			this->groupBox1->TabStop = false;
 			this->groupBox1->Text = L"AVL Tree";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(6, 83);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(60, 20);
+			this->label3->TabIndex = 9;
+			this->label3->Text = L"Delete:";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(6, 63);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(64, 20);
+			this->label2->TabIndex = 8;
+			this->label2->Text = L"Search:";
 			// 
 			// label1
 			// 
@@ -141,11 +183,91 @@ namespace CIS376CompareTreesTermProject {
 			this->label1->TabIndex = 7;
 			this->label1->Text = L"Insert:";
 			// 
+			// groupBox2
+			// 
+			this->groupBox2->Controls->Add(this->label6);
+			this->groupBox2->Controls->Add(this->label5);
+			this->groupBox2->Controls->Add(this->label4);
+			this->groupBox2->Location = System::Drawing::Point(172, 56);
+			this->groupBox2->Name = L"groupBox2";
+			this->groupBox2->Size = System::Drawing::Size(154, 148);
+			this->groupBox2->TabIndex = 7;
+			this->groupBox2->TabStop = false;
+			this->groupBox2->Text = L"BST";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(6, 83);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(60, 20);
+			this->label6->TabIndex = 2;
+			this->label6->Text = L"Delete:";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(6, 63);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(64, 20);
+			this->label5->TabIndex = 1;
+			this->label5->Text = L"Search:";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(6, 43);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(54, 20);
+			this->label4->TabIndex = 0;
+			this->label4->Text = L"Insert:";
+			// 
+			// groupBox3
+			// 
+			this->groupBox3->Controls->Add(this->label9);
+			this->groupBox3->Controls->Add(this->label7);
+			this->groupBox3->Controls->Add(this->label8);
+			this->groupBox3->Location = System::Drawing::Point(332, 56);
+			this->groupBox3->Name = L"groupBox3";
+			this->groupBox3->Size = System::Drawing::Size(154, 148);
+			this->groupBox3->TabIndex = 8;
+			this->groupBox3->TabStop = false;
+			this->groupBox3->Text = L"Splay";
+			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->Location = System::Drawing::Point(6, 80);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(60, 20);
+			this->label9->TabIndex = 10;
+			this->label9->Text = L"Delete:";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(7, 39);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(54, 20);
+			this->label7->TabIndex = 0;
+			this->label7->Text = L"Insert:";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Location = System::Drawing::Point(6, 60);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(64, 20);
+			this->label8->TabIndex = 9;
+			this->label8->Text = L"Search:";
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1259, 1055);
+			this->ClientSize = System::Drawing::Size(493, 440);
+			this->Controls->Add(this->groupBox3);
+			this->Controls->Add(this->groupBox2);
 			this->Controls->Add(this->groupBox1);
 			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->textBox2);
@@ -155,9 +277,13 @@ namespace CIS376CompareTreesTermProject {
 			this->Controls->Add(this->button1);
 			this->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"CompareTrees";
 			this->groupBox1->ResumeLayout(false);
 			this->groupBox1->PerformLayout();
+			this->groupBox2->ResumeLayout(false);
+			this->groupBox2->PerformLayout();
+			this->groupBox3->ResumeLayout(false);
+			this->groupBox3->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
